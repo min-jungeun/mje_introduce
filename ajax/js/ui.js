303e83565d712ab7f574ca4b712270635b7fb451
min-jungeun <mje971115@gmail.com>
2024-06-24 17:41:53 +0900

    scroll 레이아웃 scss 설정

--- ajax/js/ui.js
+++ ajax/js/ui.js
@@ -2,9 +2,9 @@ $(function(){
     $(window).on('scroll', function(){ //this 개념 조심하기 => 는 this 못씀
         let scrpos = $(this).scrollTop();
         if(scrpos > 80 ){ //스크롤이 아래로
-            $("#hd").addClass('bg-dark border-bottom border-white').removeClass("bg-white")
+            $("#hd").addClass('scroll')
         }else{
-            $("#hd").removeClass('bg-dark border-bottom border-white').addClass("bg-white")
+            $("#hd").removeClass('scroll')
         }
         // $("body").attr("data-scrollps", $(this).scrollTop() )
     })
